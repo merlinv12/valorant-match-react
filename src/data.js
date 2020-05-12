888d4b309903ebb75a51c2e8ea0940d5e7615a48
Anish Patel <apatel010@gmail.com>
2020-05-12 14:28:45 -0700

    added a few more randomization things

--- src/data.js
+++ src/data.js
@@ -1,109 +1,60 @@
-// let fakeData = {
-//     "matchID": 1,
-//     "greenTeamRoundWins": 12,
-//     "redTeamRoundWins": 13,
-//     "map": "Bind",
-//     "rounds": [
-//         {
-//             "roundId": "asdf123-UID",
-//             "roundNum": 1,
-//             "roundVictory": "green",
-//             "resultCondition": "Elimination",
-//             "greenEcon": 700,
-//             "redEcon":1000,
-//             "players": [
-//                 {
-//                     "playerID": 1112,
-//                     "score": 52,
-//                     "kills": 1,
-//                     "assists": 1,
-//                     "econBank": 700,
-//                     "econLoadout": 100,
-//                     "dmg": 0,
-//                     "equip": {
-//                         "gun": "Classic",
-//                         "shield": "Light Shields"
-//                     }
-//                 }
-//             ],
-//             "events": []
-//         },
-//         {
-//             "roundId": "asdf123-UID",
-//             "roundNum": 2,
-//             "roundVictory": "green",
-//             "resultCondition": "Elimination",
-//             "greenEcon": 2000,
-//             "redEcon": 7000,
-//             "players": [
-//                 {
-//                     "playerID": 1112,
-//                     "score": 52,
-//                     "kills": 1,
-//                     "assists": 1,
-//                     "econBank": 700,
-//                     "econLoadout": 100,
-//                     "dmg": 0,
-//                     "equip": {
-//                         "gun": "Classic",
-//                         "shield": "Light Shields"
-//                     }
-//                 }
-//             ],
-//             "events": []
-//         }
-//     ]
-// }
-
-// export default fakeData;
-
-
-
-function createFakeMatch(){
-    let match = {};
-    match.greenTeamRoundWins = 0;
-    match.redTeamRoundWins = 0;
-    match.rounds = [];
-    for (let i = 0; i < 26; i++){
-        if (match.greenTeamRoundWins === 13 || match.redTeamRoundWins === 13) break;
-        let round = {};
-        round.roundNum = i+1;
-        Math.floor(Math.random() * 2 ) === 1 ? round.roundVictory = "green" : round.roundVictory = 'red';
-        round.roundVictory === 'green' ? match.greenTeamRoundWins++ : match.redTeamRoundWins++;
-        round.resultCondition = ['Elimination', 'Defuse', 'Spike', 'Time'][Math.floor(Math.random() * 4)]
-        round.players = [];
-        for (let j = 1; j < 11; j++){
-            let player = {};
-            player.playerID = j;
-            player.agent = "Sova"
-            player.playerName = "player"+j
-            player.score = Math.floor(Math.random() * 100)
-            player.kills = Math.floor(Math.random() * 2)
-            player.assists = Math.floor(Math.random() * 2)
-            player.econBank = Math.floor((Math.random() * 12000) / 100) * 100;
-            player.econLoadout = Math.floor((Math.random() * 6000) / 100) * 100;
-            player.equip = {
-                "gun": "Classic",
-                "shield": "Light Shields"
-            }
-            round.players.push(player)
-        }
-        round.greenEcon = round.players.reduce((total, player) => {
-            if (player.playerID < 6 ){
-                total+= player.econLoadout
-            }
-            return total;
-        },0)
-        round.redEcon = round.players.reduce((total, player) => {
-            if (player.playerID >= 6 ){
-                total+= player.econLoadout
-            }
-            return total;
-        },0)
-        round.events = [];
-        match.rounds.push(round)
+function createFakeMatch() {
+  let match = {};
+  match.greenTeamRoundWins = 0;
+  match.redTeamRoundWins = 0;
+  match.rounds = [];
+  for (let i = 0; i < 26; i++) {
+    if (match.greenTeamRoundWins === 13 || match.redTeamRoundWins === 13) break;
+    let round = {};
+    round.roundNum = i + 1;
+    generateRandom(2) === 1
+      ? (round.roundVictory = "green")
+      : (round.roundVictory = "red");
+    round.roundVictory === "green"
+      ? match.greenTeamRoundWins++
+      : match.redTeamRoundWins++;
+    round.resultCondition = ["Elimination", "Defuse", "Spike", "Time"][
+      generateRandom(4)
+    ];
+    round.players = [];
+    for (let j = 1; j < 11; j++) {
+      let player = {};
+      player.playerID = j;
+      player.agent = ["Sova", "Raze", "Sage", "Breach", "Jett"][
+        generateRandom(5)
+      ];
+      player.playerName = "player" + j;
+      player.score = generateRandom(100);
+      player.kills = generateRandom(2);
+      player.assists = generateRandom(2);
+      player.econBank = Math.floor(generateRandom(9000) / 100) * 100;
+      player.econLoadout = Math.floor(generateRandom(6000) / 100) * 100;
+      player.equip = {
+        gun: ["Classic", "Ghost", "Phantom", "Vandal"][generateRandom(4)],
+        shield: ["Light Shields", "Heavy Shields"][generateRandom(2)],
+      };
+      round.players.push(player);
     }
-    return match;
+    round.greenEcon = round.players.reduce((total, player) => {
+      if (player.playerID < 6) {
+        total += player.econLoadout;
+      }
+      return total;
+    }, 0);
+    round.redEcon = round.players.reduce((total, player) => {
+      if (player.playerID >= 6) {
+        total += player.econLoadout;
+      }
+      return total;
+    }, 0);
+    round.events = [];
+    match.rounds.push(round);
+  }
+  return match;
+}
+
+function generateRandom(max) {
+  return Math.floor(Math.random() * max);
 }
 
 let fakeData = createFakeMatch();
