054636655ffdfc81b5b2fc8299fca06ea6dbc5e0
Anish Patel <apatel010@gmail.com>
2020-12-02 16:50:02 -0800

    converted App and Match components to functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import Match from './Match.jsx';
 
-class App extends React.Component{
-
-    render(){
-        return (
-            <div>
-                <Match />
-            </div>
-        )
-    }
-}
-
+const App = () => (
+    <div>
+        <Match />
+    </div>
+)
 export default App;
